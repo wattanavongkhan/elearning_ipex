7b992f1789e231d4735014a0aa7bebedfa830f6f
wattava58 <wattanavongkhan@gmail.com>
2026-03-24 08:09:51 +0700

    Commit : Addition function

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -19,6 +19,7 @@ class CourseController extends Controller
      */
     public function index()
     {
+        
         if (Auth::user()->status=="1") {
             return redirect()->route('home');
         }else{
